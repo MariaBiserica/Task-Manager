bb56c74b8b341c6697710dc27bfaba110ebaefb5
MariaBiserica <mariabiserica@gmail.com>
2023-04-22 21:18:32 +0300

    Added Task and TDL models.

---
+++ Task_Manager/Task_Manager/Models/TDL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Manager.Models
+{
+    internal class TDL
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public List<object> Content { get; set; }
+    }
+}
---
+++ Task_Manager/Task_Manager/Models/Task.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Manager.Models
+{
+    internal class Task
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public TaskStatus Status { get; set; }
+        public TaskPriority Priority { get; set; }
+        public DateTime Deadline { get; set; }
+        public DateTime CompletionDate { get; set; }
+        public string Category { get; set; }
+
+        public enum TaskStatus
+        {
+            Created,
+            InProgress,
+            Done
+        }
+
+        public enum TaskPriority
+        {
+            Low,
+            Medium,
+            High
+        }
+    }
+}
