315b2d62fa27e6faf602bcf941ffb3007a867249
MariaBiserica <mariabiserica@gmail.com>
2023-04-28 02:58:12 +0300

    Added more icon images to resources.

--- Task_Manager/Task_Manager/ViewModels/TreeViewVM.cs
+++ Task_Manager/Task_Manager/ViewModels/TreeViewVM.cs
@@ -52,24 +52,30 @@ namespace Task_Manager.ViewModels
             TDL tdl1 = new TDL
             {
                 Name = "TDL 1",
-                Icon = "../Resources/Icons/folder.jpg",
+                Icon = "../Resources/Icons/folder1.jpg",
                 SubCollection = new ObservableCollection<TDL>()
                 {
-                    new TDL { Name = "Sub-TDL 1" },
-                    new TDL { Name = "Sub-TDL 2" },
-                    new Task { Name = "Task 1" },
-                    new Task { Name = "Task 2" }
+                    new TDL { Name = "Sub-TDL 1", Icon = "../Resources/Icons/book1.jpg", 
+                        SubCollection = new ObservableCollection<TDL>()
+                        {
+                            new TDL() { Name = "Sub-Sub-TDL 1", Icon = "../Resources/Icons/search1.jpg" },
+                            new TDL() { Name = "Sub-Sub-TDL 2", Icon = "../Resources/Icons/settings1.jpg"}
+                        } 
+                    },
+                    new TDL { Name = "Sub-TDL 2", Icon = "../Resources/Icons/book2.jpg"},
+                    new Task { Name = "Task 1", Icon = "../Resources/Icons/checklist1.jpg" },
+                    new Task { Name = "Task 2", Icon = "../Resources/Icons/checklist2.jpg"}
                 }
-                    };
+            };
 
             TDL tdl2 = new TDL
             {
                 Name = "TDL 2",
-                Icon = "../Resources/Icons/folder.jpg",
+                Icon = "../Resources/Icons/folder2.jpg",
                 SubCollection = new ObservableCollection<TDL>()
                 {
-                    new Task { Name = "Task 3" },
-                    new Task { Name = "Task 4" }
+                    new Task { Name = "Task 3", Icon = "../Resources/Icons/docs1.jpg" },
+                    new Task { Name = "Task 4", Icon = "../Resources/Icons/computer1.jpg"}
                 }
             };
 
