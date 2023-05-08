40569f36c638a12f0efb5eaa77798dad505f996a
MariaBiserica <mariabiserica@gmail.com>
2023-05-09 01:51:54 +0300

    Modified MoveDownCommand for TDL.

--- Task_Manager/Task_Manager/Commands/MoveDownCommand.cs
+++ Task_Manager/Task_Manager/Commands/MoveDownCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Input;
 using Task_Manager.Models;
@@ -22,43 +23,81 @@ namespace Task_Manager.Commands
 
         public void Execute(object parameter)
         {
-            var selectedTDL = _viewModel.SelectedTDL;
-
-            if (_viewModel.Data.ItemsCollection.Contains(selectedTDL))
+            // Check if SelectedTDL is part of ItemsCollection
+            if (_viewModel.Data.ItemsCollection.Contains(_viewModel.SelectedTDL))
             {
-                var index = _viewModel.Data.ItemsCollection.IndexOf(selectedTDL);
+                // Get index of SelectedTDL in ItemsCollection
+                int index = _viewModel.Data.ItemsCollection.IndexOf(_viewModel.SelectedTDL);
+                // Check if SelectedTDL is not already the last of ItemsCollection
                 if (index < _viewModel.Data.ItemsCollection.Count - 1)
                 {
-                    _viewModel.Data.ItemsCollection.RemoveAt(index);
-                    _viewModel.Data.ItemsCollection.Insert(index + 1, selectedTDL);
+                    // Swap SelectedTDL with TDL above it in ItemsCollection
+                    _viewModel.Data.ItemsCollection[index] = _viewModel.Data.ItemsCollection[index + 1];
+                    _viewModel.Data.ItemsCollection[index + 1] = _viewModel.SelectedTDL;
                 }
                 else
                 {
-                    MessageBox.Show("This is already the last item in the list.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    MessageBox.Show("Cannot move TDL down any further.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
             else
             {
-                foreach (var tdl in _viewModel.Data.ItemsCollection)
+                // SelectedTDL is part of a SubCollection
+                bool found = false;
+                foreach (TDL parentTDL in _viewModel.Data.ItemsCollection)
                 {
-                    if (tdl.SubCollection.Contains(selectedTDL))
+                    if (found)
                     {
-                        var index = tdl.SubCollection.IndexOf(selectedTDL);
-                        if (index < tdl.SubCollection.Count - 1)
-                        {
-                            tdl.SubCollection.RemoveAt(index);
-                            tdl.SubCollection.Insert(index + 1, selectedTDL);
-                            break;
-                        }
-                        else
-                        {
-                            MessageBox.Show("This is already the last item in the list.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                        }
+                        break;
+                    }
+                    found = MoveDownRecursive(parentTDL.SubCollection, _viewModel.SelectedTDL);
+                }
+                if (!found)
+                {
+                    MessageBox.Show("Could not find TDL in data structure.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+
+            // Refresh view
+            _viewModel.NotifyPropertyChanged("SelectedTDL");
+            _viewModel.NotifyPropertyChanged("ItemsCollection");
+            _viewModel.NotifyPropertyChanged("SubCollection");
+        }
+        
+        public event EventHandler CanExecuteChanged;
+
+        private bool MoveDownRecursive(ObservableCollection<TDL> subCollection, TDL selectedTDL)
+        {
+            bool found = false;
+            for (int i = 0; i < subCollection.Count; i++)
+            {
+                TDL tdl = subCollection[i];
+                if (tdl == selectedTDL)
+                {
+                    // Check if TDL is not already the last of SubCollection
+                    if (i < subCollection.Count - 1)
+                    {
+                        // Swap TDL with TDL above it in SubCollection
+                        subCollection[i] = subCollection[i + 1];
+                        subCollection[i + 1] = selectedTDL;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Cannot move TDL down any further.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    found = true;
+                    break;
+                }
+                else if (tdl.SubCollection.Count > 0)
+                {
+                    found = MoveDownRecursive(tdl.SubCollection, selectedTDL);
+                    if (found)
+                    {
+                        break;
                     }
                 }
             }
+            return found;
         }
-
-        public event EventHandler CanExecuteChanged;
     }
 }
